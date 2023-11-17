2d0b4987ec2a5d9198e9ea7f78ec4e7802fbfdb0
bachidze <bachidze90@gmail.com>
2023-11-17 21:15:29 +0400

    third commit

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let toggle = document.querySelector('.inside')
 let price = document.querySelectorAll('h1')
+let meoreprice = document.querySelectorAll('h5')
+let mesameprice = document.querySelectorAll('h6')
 let fasi = 19.9
 toggle.addEventListener('click',()=>{
     if(fasi < 40){
@@ -18,6 +20,43 @@ toggle.addEventListener('click',()=>{
     });
 
     toggle.classList.toggle('rotate')
+})
+
+let meorefasi = 24.9
+toggle.addEventListener('click',()=>{
+    if(meorefasi < 40){
+        console.log('shemodis')
+        meorefasi *= 10
+    }else{
+        console.log('else')
+        meorefasi/=10
+    }
+
+    console.log(meorefasi)
+    meoreprice.innerHTML = meorefasi
+
+    meoreprice.forEach(meoreprice => {
+        meoreprice.innerHTML = meorefasi;
+    });
+   
+})
+let mesamefasi = 39.9
+toggle.addEventListener('click',()=>{
+    if(mesamefasi < 40){
+        console.log('shemodis')
+        mesamefasi *= 10
+    }else{
+        console.log('else')
+        mesamefasi/=10
+    }
+
+    console.log(mesamefasi)
+    mesameprice.innerHTML = mesamefasi
+
+    mesameprice.forEach(mesameprice => {
+        mesameprice.innerHTML = mesamefasi;
+    });
+   
 })
 
 
